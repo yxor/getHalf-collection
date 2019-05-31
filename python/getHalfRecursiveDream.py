dca23b79af26a33970271fd10c6e8b11f544364e
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2019-05-31 02:03:57 +0100

    adding a **very slow** recursive example

---
+++ python/getHalfRecursiveDream.py
@@ -0,0 +1,25 @@
+"""using recursivity to find the half of an integers, only works for positives"""
+
+def divideBy2(n):
+    half = 0
+    while n >= 0:
+        n = n-2
+        half += 1
+    return half - 1
+
+def half(n):
+    if n == 1:
+        return 0
+    if divideBy2(n) == 1:
+        return 1
+    return half(divideBy2(n)) + half(divideBy2(n) + 1)
+
+def getHalf(n):
+    if n == 1:
+        return 0
+    if half(n)  == 1:
+        return 1
+    return getHalf(half(n)) + getHalf(half(n) + 1)
+
+getHalf(20)
+getHalf(500000)
