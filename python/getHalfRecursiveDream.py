5dcdc663418811c530d4c81218661eeaf673de18
Ahmed Tounsi <ahmeddottounsi@gmail.com>
2019-05-31 04:04:31 +0100

    slowing down the algorithm even more

--- python/getHalfRecursiveDream.py
+++ python/getHalfRecursiveDream.py
@@ -1,25 +1,28 @@
 """using recursivity to find the half of an integers, only works for positives"""
 
-def divideBy2(n):
+# setting the recursive dream depth to an arbitrary number
+# the bigger the number the longer it will take to find the half
+MAX_DEPTH = 15
+
+
+def base(n, depth=0):
     half = 0
     while n >= 0:
         n = n-2
         half += 1
     return half - 1
 
-def half(n):
+def half(n, depth):
+    div_func = half
+    if depth <= 0:
+        div_func = base
     if n == 1:
         return 0
-    if divideBy2(n) == 1:
+    if div_func(n, depth-1) == 1:
         return 1
-    return half(divideBy2(n)) + half(divideBy2(n) + 1)
+    return half(div_func(n, depth -1), depth-1) + half(div_func(n, depth -1) + 1, depth-1)
 
-def getHalf(n):
-    if n == 1:
-        return 0
-    if half(n)  == 1:
-        return 1
-    return getHalf(half(n)) + getHalf(half(n) + 1)
 
-getHalf(20)
-getHalf(500000)
+# with a depth of 15, it takes the function 75 seconds to find the half of 2
+half(4, MAX_DEPTH)
+
